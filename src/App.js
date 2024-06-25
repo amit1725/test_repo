93b7d898033a67da97528d6e929830a186716e64
Sonam Kadam <sonam25kadam@gmail.com>
2024-06-25 14:38:58 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./SCREENS/Home";
+import Login from "./SCREENS/Login";
+
+import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css'  //npm i bootstrap-dark-5 boostrap
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+import 'bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import Signup from "./SCREENS/Signup.js";
+import { CartProvider } from "./components/ContextReducer.js";
+import MyOrder from "./SCREENS/MyOrder.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CartProvider>
+      <Router>
+      <div className="main">
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route exact path="/login" element={<Login/>} />
+          <Route exact path="/createuser" element={<Signup/>} />
+          <Route exact path="/myOrder" element={<MyOrder/>} />
+        </Routes>
+      </div>
+    </Router>
+    </CartProvider>
   );
 }
 
