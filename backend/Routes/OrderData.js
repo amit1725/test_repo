da72db9164a1e708203dadeadf2c55c1f2389c91
Sonam Kadam <125948681+sonamkadam25@users.noreply.github.com>
2024-07-02 05:57:32 +0530

    Update OrderData.js

--- backend/Routes/OrderData.js
+++ backend/Routes/OrderData.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Order = require("../models/Orders");
 
-router.post("/orderData", async (req, res) => {
+router.post("/orderData", async (req, res) => { 
   try {
     let data = req.body.order_data;
     if (!data || !req.body.email) {
